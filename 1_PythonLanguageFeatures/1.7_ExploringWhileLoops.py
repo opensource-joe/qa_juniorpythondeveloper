aa19dff78322a3919011f3ab2429600a511cfba7
opensource-joe <joecastleengineering@gmail.com>
2025-05-06 17:13:26 -0400

    updates to files

---
+++ 1_PythonLanguageFeatures/1.7_ExploringWhileLoops.py
@@ -0,0 +1,4 @@
+# Exploring While Loops
+# While loops are used to execute a block of code as long as a condition is true.
+# They are useful when the number of iterations is not known beforehand.
+
